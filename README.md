7066e88d92b711c0ed1c8bfb8ec56f04091cdd22
Mahmoud Atef <mahmoud.atef191996@gmail.com>
2025-09-17 11:59:04 +0300

    simple print

---
+++ README.md
@@ -0,0 +1,1 @@
+New pythion project
---
+++ basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+sum = number1 + number2
+sub = number2 - number1
+profuct = number1 * number2
+print (f"Addition of {number1} and {number2} is {sum}")
+print (f"Subtraction  of {number2} and {number1} is {sub}")
+print (f"Multiplication  of {profuct} and {number2} is {profuct}")
