b901efa7acbb512fc65a210cba842aad95c552e8
Mahmoud Atef <mahmoud.atef191996@gmail.com>
2025-10-05 22:13:53 +0300

    try

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime, timedelta
 def display_current_datetime():
     current_date = datetime.datetime.now()
     print(f"Current date and time: {current_date}")
