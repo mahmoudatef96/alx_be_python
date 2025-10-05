09db667e6f55f67011114be192954e6ff3c76704
Mahmoud Atef <mahmoud.atef191996@gmail.com>
2025-10-06 00:03:48 +0300

    errors

---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,13 @@
+def safe_divide(numerator, denominator):
+    try:
+        num = float(numerator)
+        den = float(denominator)
+        try:                
+            result = num / den
+        except ZeroDivisionError:
+            return("\nError: Cannot divide by zero." )
+        return(f"The result of {numerator} / {denominator} is: {result}")
+    except ValueError:
+        return("\nError: Please enter numeric values only.")
+   
+    
