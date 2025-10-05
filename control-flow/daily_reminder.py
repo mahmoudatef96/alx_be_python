b58836aaf8b5863fa764f2986b8cdbbf4c913a8d
Mahmoud Atef <mahmoud.atef191996@gmail.com>
2025-10-05 17:40:04 +0300

    remider

---
+++ control-flow/daily_reminder.py
@@ -0,0 +1,19 @@
+task = input("Enter your task: ")
+priority = input("Priority (high/medium/low): ")
+time = input("Is it time-bound? (yes/no): ")
+match priority :
+    case "high" :
+        if time == "yes" :
+            print(f"Reminder: '{task}' is a high priority task that requires immediate attention today!")
+        else :
+            print(f"Note: '{task}' is a high priority task that requires immediate attention ASAP")
+    case "medium" :
+        if time == "yes" :
+            print(f"Reminder: '{task}' is a medium priority task need to be finished ASAP")
+        else :
+            print(f"Note: '{task}' is a medium priority task")
+    case "low":
+        if time == "yes" :
+            print(f"Reminder: '{task}' is a low priority task need to be finished when you can")
+        else :
+            print(f"Note: '{task}' is a low priority task. Consider completing it when you have free time.")
