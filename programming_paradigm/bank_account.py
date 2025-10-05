19344da95ff7ab8ff5203193d3b0da3d1aa646da
Mahmoud Atef <mahmoud.atef191996@gmail.com>
2025-10-05 23:24:16 +0300

    classes

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,7 +1,7 @@
 class BankAccount:
-    
+    ccount_balance = 0
     def __init__(self, account_balance):
-        self.account_balance = 0
+        self.account_balance = account_balance
 
     def deposit(self,amount):
         self.account_balance += amount
